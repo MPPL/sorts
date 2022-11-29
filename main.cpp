476de37ac24d07808f572fc46165facd21b251fd
MPPL <mastermplayp@gmail.com>
2022-11-29 10:10:07 +0100

    clearing

--- main.cpp
+++ main.cpp
@@ -6,13 +6,11 @@
 
 void merge(int array[], int start, int mid, int end){
 
-    //printf("%d==%d==%d\n", start, mid, end);
     int left = start;
     int right = mid+1;
     int i = start;
     int arr[N];
     while((left<= mid)&&(right <= end)){
-        //printf("%d ??? %d ??? %d\n", start, mid, end);
         if(array[left]<array[right]){
             arr[i] = array[left];
             left++;
@@ -23,26 +21,19 @@ void merge(int array[], int start, int mid, int end){
         }
         i++;
     }
-    //printf("%d--%d\n", i, arr[i-1]);
     while(left<= mid){
         arr[i] = array[left];
         left++;
         i++;
     }
-    //printf("%d--%d\n", i, arr[i-1]);
     while(right<=end){
         arr[i] = array[right];
         right++;
         i++;
     }
-    //printf("%d--%d\n", i, arr[i-1]);
     for(int j = start; j <= end; j++){
         array[j] = arr[j];
     }
-    
-    //printf("%d--%d--%d\n", start, end-start, i);
-    //printf("%d\n",arr);
-    //delete arr;
     
 }
 
@@ -52,7 +43,6 @@ void mergesort(int array[], int start, int end){
         mergesort(array,start,mid);
         mergesort(array,mid+1,end);
         merge(array, start, mid, end);
-        //printf("%d--%d\n", end, 100);
     }
 }
 
